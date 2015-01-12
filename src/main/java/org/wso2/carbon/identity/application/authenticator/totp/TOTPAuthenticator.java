d4be88e8181144385f0ecba1ad85c3808eb8b2ff
Lasitha Wattaladeniya <wattale@gmail.com>
2015-01-12 11:34:17 +0530

    adding code

--- src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPAuthenticator.java
+++ src/main/java/org/wso2/carbon/identity/application/authenticator/totp/TOTPAuthenticator.java
@@ -1,7 +1,117 @@
 package org.wso2.carbon.identity.application.authenticator.totp;
 
-/**
- * Created by lasitha on 1/12/15.
- */
-public class TOTPAuthenticator {
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.application.authentication.framework.AbstractApplicationAuthenticator;
+import org.wso2.carbon.identity.application.authentication.framework.AuthenticatorFlowStatus;
+import org.wso2.carbon.identity.application.authentication.framework.LocalApplicationAuthenticator;
+import org.wso2.carbon.identity.application.authentication.framework.config.ConfigurationFacade;
+import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
+import org.wso2.carbon.identity.application.authentication.framework.exception.AuthenticationFailedException;
+import org.wso2.carbon.identity.application.authentication.framework.exception.InvalidCredentialsException;
+import org.wso2.carbon.identity.application.authentication.framework.exception.LogoutFailedException;
+import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkUtils;
+import org.wso2.carbon.identity.base.IdentityException;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
+import org.wso2.carbon.user.api.UserRealm;
+import org.wso2.carbon.user.core.UserStoreManager;
+import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TOTPAuthenticator extends AbstractApplicationAuthenticator
+        implements LocalApplicationAuthenticator {
+
+    private static final long serialVersionUID = 4438354156955223654L;
+
+    private static Log log = LogFactory.getLog(TOTPAuthenticator.class);
+
+    @Override
+    public boolean canHandle(HttpServletRequest request) {
+
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (userName != null && password != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public AuthenticatorFlowStatus process(HttpServletRequest request,
+                                           HttpServletResponse response, AuthenticationContext context)
+            throws AuthenticationFailedException, LogoutFailedException {
+
+        if (context.isLogoutRequest()) {
+            return AuthenticatorFlowStatus.SUCCESS_COMPLETED;
+        } else {
+            return super.process(request, response, context);
+        }
+    }
+
+    @Override
+    protected void initiateAuthenticationRequest(HttpServletRequest request,
+                                                 HttpServletResponse response, AuthenticationContext context)
+            throws AuthenticationFailedException {
+
+        String loginPage = ConfigurationFacade.getInstance().getAuthenticationEndpointURL();
+        String queryParams = FrameworkUtils
+                .getQueryStringWithFrameworkContextId(context.getQueryParams(),
+                                                      context.getCallerSessionKey(),
+                                                      context.getContextIdentifier());
+
+        try {
+            String retryParam = "";
+
+            if (context.isRetrying()) {
+                retryParam = "&authFailure=true&authFailureMsg=login.fail.message";
+            }
+
+            response.sendRedirect(response.encodeRedirectURL(loginPage + ("?" + queryParams))
+                                  + "&authenticators=" + getName() + ":" + "LOCAL" + retryParam);
+        } catch (IOException e) {
+            throw new AuthenticationFailedException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected void processAuthenticationResponse(HttpServletRequest request,
+                                                 HttpServletResponse response, AuthenticationContext context)
+            throws AuthenticationFailedException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        boolean isAuthenticated = false;
+
+
+    }
+
+    @Override
+    protected boolean retryAuthenticationEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getContextIdentifier(HttpServletRequest request) {
+        return request.getParameter("sessionDataKey");
+    }
+
+    @Override
+    public String getFriendlyName() {
+        return "BasicAuthenticatorConstants.AUTHENTICATOR_FRIENDLY_NAME";
+    }
+
+    @Override
+    public String getName() {
+        return "BasicAuthenticatorConstants.AUTHENTICATOR_NAME";
+    }
+
+
+
 }
